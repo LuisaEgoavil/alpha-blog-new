2302cd9816b91392b936082fec132e8485aac6c3
Luisa Egoavil <lupe.egoavil@gmail.com>
2022-04-07 11:15:05 +0200

    integration test and feature: listing categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,8 @@ class CategoriesController < ApplicationController
     end
 
     def index 
+        # pagination
+        @categories = Category.paginate(page: params[:page], per_page: 5)
     end 
 
     def show  
